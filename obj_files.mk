87de89fb421925a6c2d0649f22acd042021ad588
wowjinxy <wowjinxy@aol.com>
2021-08-26 10:37:18 -0700

    Split out cobj and renamed all the unsplit text to better understand their location as time goes on

--- obj_files.mk
+++ obj_files.mk
@@ -12,24 +12,26 @@ EXTABINDEX_O_FILES :=                               \
 TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/gmmain.o						\
 	$(BUILD_DIR)/asm/gmglobal.o						\
-	$(BUILD_DIR)/asm/text1.o						\
-	$(BUILD_DIR)/asm/text2.o						\
-	$(BUILD_DIR)/asm/text3.o						\
-	$(BUILD_DIR)/asm/text4.o						\
-	$(BUILD_DIR)/asm/text5.o						\
-	$(BUILD_DIR)/asm/text6.o						\
-	$(BUILD_DIR)/asm/text7.o						\
+	$(BUILD_DIR)/asm/text_80007808.o				\
+	$(BUILD_DIR)/asm/text_80010BBC.o				\
+	$(BUILD_DIR)/asm/text_80060ED0.o				\
+	$(BUILD_DIR)/asm/text_801166B4.o				\
+	$(BUILD_DIR)/asm/text_801D443C.o				\
+	$(BUILD_DIR)/asm/text_8028B974.o				\
+	$(BUILD_DIR)/asm/text_8034D384.o				\
 	$(BUILD_DIR)/asm/__va_arg.o						\
-	$(BUILD_DIR)/asm/text7-1.o						\
+	$(BUILD_DIR)/asm/text_803AD168.o				\
 	$(BUILD_DIR)/asm/abort_exit.o					\
-	$(BUILD_DIR)/asm/text7-1.1.o					\
+	$(BUILD_DIR)/asm/text_803AFAC8.o				\
 	$(BUILD_DIR)/asm/cstring.o						\
-	$(BUILD_DIR)/asm/text7-2.o						\
+	$(BUILD_DIR)/asm/text_803B4AC0.o				\
 	$(BUILD_DIR)/asm/dolphin_sdk_1.o				\
 	$(BUILD_DIR)/asm/__ppc_eabi_init.o				\
 	$(BUILD_DIR)/asm/dolphin_sdk_2.o				\
-	$(BUILD_DIR)/asm/text7-2.1.o					\
-	$(BUILD_DIR)/asm/text8.o						\
+	$(BUILD_DIR)/asm/text_803EC02C.o				\
+	$(BUILD_DIR)/asm/cobj.o							\
+	$(BUILD_DIR)/asm/text_80403594.o				\
+	$(BUILD_DIR)/asm/text_8041B450.o				\
 													\
 
 CTORS_O_FILES :=                                    \
