2807b341f5e8dd42176f807e97f25635ce4a19b3
wowjinxy <wowjinxy@aol.com>
2021-08-27 18:39:46 -0700

    Cleaned up a bit of C, removed unneeded scripts

--- asm/bss.s
+++ asm/bss.s
@@ -701,8 +701,8 @@ lbl_805892FC:
 .global lbl_805893BC
 lbl_805893BC:
 	.skip 0x5C4
-.global lbl_80589980
-lbl_80589980:
+.global alloc_data
+alloc_data:
 	.skip 0x30
 .global lbl_805899B0
 lbl_805899B0:
