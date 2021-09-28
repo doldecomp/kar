43b0236bddcb61d8c8f9d80d6c315593120e6599
wowjinxy <wowjinxy@aol.com>
2021-09-28 11:16:40 -0500

    Update obj_files.mk

--- obj_files.mk
+++ obj_files.mk
@@ -1,7 +1,7 @@
 # Linker order for every file, passed to the Metrowerks linker.
 
-INIT_O_FILES := 						            \
-    $(BUILD_DIR)/asm/init.o							\
+INIT_O_FILES := 						        \
+    $(BUILD_DIR)/asm/init.o						\
 
 EXTAB_O_FILES :=                                    \
     $(BUILD_DIR)/asm/_extab.o						\
@@ -9,9 +9,9 @@ EXTAB_O_FILES :=                                    \
 EXTABINDEX_O_FILES :=                               \
     $(BUILD_DIR)/asm/_exidx.o						\
 
-TEXT_O_FILES := 						            \
-	$(BUILD_DIR)/asm/gmmain.o						\
-	$(BUILD_DIR)/asm/gmglobal.o						\
+TEXT_O_FILES := 						        \
+	$(BUILD_DIR)/asm/gmmain.o					\
+	$(BUILD_DIR)/asm/gmglobal.o					\
 	$(BUILD_DIR)/asm/text_80007808.o				\
 	$(BUILD_DIR)/asm/text_80010BBC.o				\
 	$(BUILD_DIR)/asm/text_80060ED0.o				\
@@ -19,50 +19,50 @@ TEXT_O_FILES := 						            \
 	$(BUILD_DIR)/asm/text_801D443C.o				\
 	$(BUILD_DIR)/asm/text_8028B974.o				\
 	$(BUILD_DIR)/asm/text_8034D384.o				\
-	$(BUILD_DIR)/asm/__va_arg.o						\
+	$(BUILD_DIR)/asm/__va_arg.o					\
 	$(BUILD_DIR)/asm/text_803AD168.o				\
 	$(BUILD_DIR)/asm/abort_exit.o					\
 	$(BUILD_DIR)/asm/text_803AFAC8.o				\
-	$(BUILD_DIR)/asm/cstring.o						\
+	$(BUILD_DIR)/asm/cstring.o					\
 	$(BUILD_DIR)/asm/text_803B4AC0.o				\
 	$(BUILD_DIR)/asm/dolphin_sdk_1.o				\
 	$(BUILD_DIR)/asm/__ppc_eabi_init.o				\
 	$(BUILD_DIR)/asm/dolphin_sdk_2.o				\
 	$(BUILD_DIR)/asm/text_803EC02C.o				\
-	$(BUILD_DIR)/asm/cobj.o							\
-	$(BUILD_DIR)/asm/fobj.o							\
+	$(BUILD_DIR)/asm/cobj.o						\
+	$(BUILD_DIR)/asm/fobj.o						\
 	$(BUILD_DIR)/src/sysdolphin/pobj.o				\
-	$(BUILD_DIR)/asm/pobj.o							\
+	$(BUILD_DIR)/asm/pobj.o						\
 	$(BUILD_DIR)/asm/text_80407EE0.o				\
-	$(BUILD_DIR)/asm/objalloc.o						\
+	$(BUILD_DIR)/asm/objalloc.o					\
 	$(BUILD_DIR)/asm/text_8041838C.o				\
 	$(BUILD_DIR)/asm/text_8041B450.o				\
-	$(BUILD_DIR)/asm/object.o						\
+	$(BUILD_DIR)/asm/object.o					\
 	$(BUILD_DIR)/asm/text_8041BF1C.o				\
 
 CTORS_O_FILES :=                                    \
-	$(BUILD_DIR)/asm/ctors.o						\
+	$(BUILD_DIR)/asm/ctors.o					\
 
 DTORS_O_FILES :=                                    \
-	$(BUILD_DIR)/asm/dtors.o						\
+	$(BUILD_DIR)/asm/dtors.o					\
 
 RODATA_O_FILES :=                                   \
-	$(BUILD_DIR)/asm/rodata.o						\
+	$(BUILD_DIR)/asm/rodata.o					\
 
 DATA_O_FILES :=                                     \
-	$(BUILD_DIR)/asm/data.o							\
+	$(BUILD_DIR)/asm/data.o						\
 
 BSS_O_FILES :=                                      \
-	$(BUILD_DIR)/asm/bss.o							\
+	$(BUILD_DIR)/asm/bss.o						\
 
 SDATA_O_FILES :=                                    \
-	$(BUILD_DIR)/asm/sdata.o						\
+	$(BUILD_DIR)/asm/sdata.o					\
 
-SBSS_O_FILES :=										\
-	$(BUILD_DIR)/asm/sbss.o							\
+SBSS_O_FILES :=								\
+	$(BUILD_DIR)/asm/sbss.o						\
 
 SDATA2_O_FILES :=                                   \
-	$(BUILD_DIR)/asm/sdata2.o						\
+	$(BUILD_DIR)/asm/sdata2.o					\
 	
-SBSS2_O_FILES :=									\
-	$(BUILD_DIR)/asm/sbss2.o						\
+SBSS2_O_FILES :=							\
+	$(BUILD_DIR)/asm/sbss2.o					\
