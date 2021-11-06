f4c8a08b15c05ffb81cb8c478fe5ea474f4d6276
wowjinxy <wowjinxy@aol.com>
2021-11-06 13:21:22 -0500

    my atempt

--- src/init/__start.c
+++ src/init/__start.c
@@ -1,5 +1,5 @@
-#include "include/dolphin/os.h"
-#include "src/init/__start.h"
+#include "dolphin/os.h"
+#include "init/__start.h"
 
 #pragma section code_type ".init"
 
@@ -180,6 +180,48 @@ asm static void __init_registers(void)
 __declspec(section ".init") extern __rom_copy_info _rom_copy_info[];
 __declspec(section ".init") extern __bss_init_info _bss_init_info[];
 
+//void __init_data(void)
+//{
+//    __rom_copy_info *dci;
+//    __bss_init_info *bii;
+//
+//    dci = _rom_copy_info;
+//    while (TRUE) {
+//		//dci = (*_rom_copy_info).size;
+//        if ((*dci).size == 0) break;
+//		dci = _rom_copy_info;
+//		if ((*dci).size && ((*dci).addr != (*dci).rom)) {
+//			memcpy((*dci).addr, (*dci).rom, (*dci).size);
+//			__flush_cache( (*dci).addr, (*dci).size );
+//		}
+//        dci++;
+//    }
+//
+//    bii = _bss_init_info;
+//    while (TRUE) {
+//        if ((*bii).size == 0) break;
+//		if ((*bii).size) {
+//			memset((*bii).addr, 0, (*bii).size);
+//		}
+//        bii++;
+//    }
+//}
+
+inline static void __copy_rom_section(void* dst, const void* src, unsigned long size)
+{
+	if (size && (dst != src)) {
+		memcpy(dst, src, size);
+		__flush_cache(dst, size);
+	}
+}
+
+inline static void __init_bss_section(void* dst, unsigned long size)
+{
+	if (size) {
+		memset(dst, 0, size);
+	}
+}
+
 void __init_data(void)
 {
     __rom_copy_info *dci;
@@ -187,22 +229,15 @@ void __init_data(void)
 
     dci = _rom_copy_info;
     while (TRUE) {
-		//dci = (*_rom_copy_info).size;
-        if ((*dci).size == 0) break;
-		dci = _rom_copy_info;
-		if ((*dci).size && ((*dci).addr != (*dci).rom)) {
-			memcpy((*dci).addr, (*dci).rom, (*dci).size);
-			__flush_cache( (*dci).addr, (*dci).size );
-		}
+        if (dci->size == 0) break;
+		__copy_rom_section(dci->addr, dci->rom, dci->size);
         dci++;
     }
 
     bii = _bss_init_info;
     while (TRUE) {
-        if ((*bii).size == 0) break;
-		if ((*bii).size) {
-			memset((*bii).addr, 0, (*bii).size);
-		}
+		if (bii->size == 0) break;
+		__init_bss_section(bii->addr, bii->size);
         bii++;
     }
 }
