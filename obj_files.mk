4cb15d4fe89e125d3ce025872f9364a7280ca63a
wowjinxy <wowjinxy@aol.com>
2021-11-06 14:37:42 -0500

    forgot to include this but now broke something somehow

--- obj_files.mk
+++ obj_files.mk
@@ -6,6 +6,7 @@ INIT_O_FILES := 						            \
     $(BUILD_DIR)/asm/TRK_MINNOW_DOLPHIN/__exception.o	\
     $(BUILD_DIR)/src/dolphin/os/__start.o			\
     $(BUILD_DIR)/src/dolphin/os/__ppc_eabi_init.o	\
+    $(BUILD_DIR)/asm/init/__ppc_eabi_init.o	\
 
 EXTAB_O_FILES :=                                    \
     $(BUILD_DIR)/asm/_extab.o						\
