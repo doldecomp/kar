d4af20b4f5018c4f3084ef639fb012c49ed580b7
wowjinxy <wowjinxy@aol.com>
2021-11-06 21:47:44 -0500

    fixed issues with __OSPSInit __OSFPRInit and __OSCacheInit

---
+++ include/init/__ppc_eabi_init.h
@@ -0,0 +1,7 @@
+#include "dolphin/types.h"
+
+#define MSR_FP               0x2000
+
+extern void __OSCacheInit   ( void );
+extern void __OSFPRInit     ( void );
+extern void __OSPSInit      ( void );
