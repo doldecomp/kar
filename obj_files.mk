81ac30efa7608f8143f3b4a678f7010540b1a057
wowjinxy <wowjinxy@aol.com>
2023-02-14 15:33:25 -0600

    split out the top level of the debug menu

--- obj_files.mk
+++ obj_files.mk
@@ -22,6 +22,7 @@ TEXT_O_FILES :=\
 	$(BUILD_DIR)/asm/gmautodemo.o\
 	$(BUILD_DIR)/asm/text_80010BBC.o\
 	$(BUILD_DIR)/asm/text_80060ED0.o\
+	$(BUILD_DIR)/asm/db_800AD79C.o\
 	$(BUILD_DIR)/asm/smsoundtest.o\
 	$(BUILD_DIR)/asm/text_800AF474.o\
 	$(BUILD_DIR)/asm/grcity1.o\
